db05a53bc116ca7df83e4c9ca900998f2683db19
MarinaCh <marina3123273@mail.ru>
2023-04-18 15:06:45 +0300

    Full coverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -18,4 +18,15 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+            public void findMaxIfEqual() {
+        StatisticsService service= new StatisticsService();
+        long[] incomesInBillions = {0, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+         long expected = 12;
+         long actual = service.findMax(incomesInBillions);
+
+         Assertions.assertEquals(expected, actual);
+    }
+
 }
